d4be25522f9eadc77b7e5a20ebfbdad26c54bd46
Anddd7 <liaoad_space@sina.com>
2019-07-01 19:20:37 +0800

    should_get_invalid_ticket_message_when_pick_given_a_invalid_ticket_with_a_car_parked_in_manager

--- src/test/java/cc/oobootcamp/parking/ParkingManagerTest.java
+++ src/test/java/cc/oobootcamp/parking/ParkingManagerTest.java
@@ -69,7 +69,7 @@ public class ParkingManagerTest {
   }
 
   @Test
-  public void should_return_a_valid_ticket_when_park_given_a_parking_manager_and_graduate_and_smart_parking_boy_are_all_full() {
+  public void should_get_cannot_park_message_when_park_given_a_parking_manager_and_graduate_and_smart_parking_boy_are_all_full() {
     IParkingBoy graduateParkingBoy = buildAFullGraduateParkingBoy();
     IParkingBoy smartParkingBoy = buildAFullSmartParkingBoy();
     ParkingManager parkingManager = new ParkingManager(
@@ -104,17 +104,50 @@ public class ParkingManagerTest {
   public void should_pick_the_car_when_pick_given_a_valid_ticket_with_a_car_parked_in_smart() {
     IParkingBoy graduateParkingBoy = buildAFullGraduateParkingBoy();
     IParkingBoy smartParkingBoy = new SmartParkingBoy(Collections.singletonList(new ParkingLot(1)));
+    Car car = new Car();
+    Ticket ticket = smartParkingBoy.park(car);
     ParkingManager parkingManager = new ParkingManager(
         Collections.singletonList(buildAFullParkingLot()),
         graduateParkingBoy,
         smartParkingBoy
     );
-    Car car = new Car();
-    Ticket ticket = parkingManager.park(car);
 
     Car pickedCar = parkingManager.pick(ticket);
 
     assertThat(pickedCar).isEqualTo(car);
+  }
+
+  @Test
+  public void should_pick_the_car_when_pick_given_a_valid_ticket_with_a_car_parked_in_graduate() {
+    IParkingBoy graduateParkingBoy = new GraduateParkingBoy(Collections.singletonList(new ParkingLot(1)));
+    Car car = new Car();
+    Ticket ticket = graduateParkingBoy.park(car);
+    IParkingBoy smartParkingBoy = buildAFullSmartParkingBoy();
+    ParkingManager parkingManager = new ParkingManager(
+        Collections.singletonList(buildAFullParkingLot()),
+        graduateParkingBoy,
+        smartParkingBoy
+    );
+
+    Car pickedCar = parkingManager.pick(ticket);
+
+    assertThat(pickedCar).isEqualTo(car);
+  }
+
+  @Test
+  public void should_get_invalid_ticket_message_when_pick_given_a_invalid_ticket_with_a_car_parked_in_manager() {
+    IParkingBoy graduateParkingBoy = new GraduateParkingBoy(Collections.singletonList(new ParkingLot(1)));
+    IParkingBoy smartParkingBoy = new SmartParkingBoy(Collections.singletonList(new ParkingLot(1)));
+    ParkingManager parkingManager = new ParkingManager(
+        Collections.singletonList(new ParkingLot(1)),
+        graduateParkingBoy,
+        smartParkingBoy
+    );
+    parkingManager.park(new Car());
+
+    assertThatThrownBy(() -> parkingManager.pick(new Ticket()))
+        .isInstanceOf(InvalidTicketException.class)
+        .hasMessage("Your ticket is invalid, cannot find your car in our park.");
   }
 
   private ParkingLot buildAFullParkingLot() {
